e9f6cf5b5191d5aee3e3f8f0804821f1b6b9bd71
Ilya Haider Muhammadi <zummerjutt12@gmail.com>
2026-05-12 23:51:28 +0100

    Fixed email crash

--- main/views.py
+++ main/views.py
@@ -8,26 +8,33 @@ def home(request):
 
     if request.method == "POST":
 
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+        try:
 
-        full_message = f"""
-        Name: {name}
-        Email: {email}
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
 
-        Message:
-        {message}
-        """
+            full_message = f"""
+            Name: {name}
+            Email: {email}
 
-        send_mail(
-            subject="New Portfolio Contact Message",
-            message=full_message,
-            from_email=email,
-            recipient_list=['ilyahaider15313@gmail.com'],
-        )
+            Message:
+            {message}
+            """
 
-        success = True
+            send_mail(
+                subject="New Portfolio Contact Message",
+                message=full_message,
+                from_email='zummerjutt12@gmail.com',
+                recipient_list=['zummerjutt12@gmail.com'],
+                fail_silently=False,
+            )
+
+            success = True
+
+        except Exception as e:
+
+            print(e)
 
     return render(request, 'index.html', {
 
